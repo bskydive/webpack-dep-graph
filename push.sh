402461018c327080befe74a31d7c2461c61fddfc
Valeriy Stepanov <stepanovv.ru@yandex.ru>
2023-12-27 17:15:26 +0300

    add files

--- push.sh
+++ push.sh
@@ -3,6 +3,6 @@
 # git remote add gh git@github.com:bskydive/webpack-dep-graph.git
 
 git add -A ./*
-git commit -am "forgot to add files"
-git push gh --all
-git push gl --all
+git commit -am "add files"
+git push gh release-v1.4.0
+git push gl release-v1.4.0
