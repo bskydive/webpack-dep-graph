ba42f4e64b34ac7346748f34fd181c53d6a4ac0b
Valeriy Stepanov <stepanovv.ru@yandex.ru>
2023-12-27 14:25:50 +0300

    forgot to add files

--- push.sh
+++ push.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# git remote add gl git@gitlab.com:stepanovv/webpack-dep-graph.git
+# git remote add gh git@github.com:bskydive/webpack-dep-graph.git
 
 git add -A ./*
 git commit -am "forgot to add files"
